063de3d1bd33d97db333150925879f73a1f3d7c0
pratsosa <alexandrospratsos@mail.utoronto.ca>
2024-11-08 11:16:32 -0500

    updated entities

--- src/main/java/entity/LeagueData.java
+++ src/main/java/entity/LeagueData.java
@@ -1,5 +1,36 @@
 package entity;
 
-public class LeagueData {
+import java.util.List;
 
+public class LeagueData {
+    private int ranking;
+    private double points;
+    private List<String> words;
+
+    public LeagueData(int ranking, double points, List<String> words) {
+        this.ranking = ranking;
+        this.points = points;
+        this.words = words;
+    }
+    public int getRanking() {
+        return ranking;
+    }
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+    public double getPoints() {
+        return points;
+    }
+    public void setPoints(double points) {
+        this.points = points;
+    }
+    public List<String> getWords() {
+        return words;
+    }
+    public void setWords(List<String> words) {
+        this.words = words;
+    }
+    public void addPoints(double points) {
+        this.points += points;
+    }
 }
