7b66b002d46e2dfe3331a6831bcfa922ce6d7b62
Evelyn Hughes <evelyn.hughes@mail.utoronto.ca>
2024-11-24 03:33:31 -0500

    switched back to main key

--- src/main/java/data_access/PantryUserDataAccessObject.java
+++ src/main/java/data_access/PantryUserDataAccessObject.java
@@ -61,7 +61,7 @@ public class PantryUserDataAccessObject implements SignupUserDataAccessInterface
         this.userFactory = userFactory;
         try {
         // if you run into an issue here, it menas that you don't have your pantry API key, text Alex to get it
-            this.pantryID = new Scanner(new File("pantryAPIkeypersonal.txt")).nextLine();
+            this.pantryID = new Scanner(new File("pantryAPIkey.txt")).nextLine();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
