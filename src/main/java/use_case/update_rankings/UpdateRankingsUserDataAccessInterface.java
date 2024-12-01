2123e85777a5a9156351e02c3dd25374f22c0832
gisimfu <h.fuhrer.gva@gmail.com>
2024-11-30 19:40:03 -0500

    continued implementing rankings testing

--- src/main/java/use_case/update_rankings/UpdateRankingsUserDataAccessInterface.java
+++ src/main/java/use_case/update_rankings/UpdateRankingsUserDataAccessInterface.java
@@ -1,10 +1,12 @@
 package use_case.update_rankings;
 
+import entity.League;
 import entity.User;
 
 import java.util.ArrayList;
 
 public interface UpdateRankingsUserDataAccessInterface {
     public User get(String username);
+    public void save(User user);
     ArrayList<User> getUsers(ArrayList<String> usernames);
 }
--- src/test/java/use_case/update_rankings/UpdateRankingsTest.java
+++ src/test/java/use_case/update_rankings/UpdateRankingsTest.java
@@ -1,14 +1,9 @@
 package use_case.update_rankings;
 
-import data_access.Constants;
 import data_access.GuardianDataAccessObject;
 import data_access.InMemoryUserDataAccessObject;
 import entity.*;
 import org.junit.jupiter.api.Test;
-import use_case.add_word.*;
-import use_case.solo_play.SoloPlayUserDataAccessInterface;
-import use_case.update_solo_points.*;
-
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,21 +12,39 @@ public class UpdateRankingsTest {
     @Test
     void successTest(){
         UpdateRankingsInputData inputData = new UpdateRankingsInputData("rankingsTestLeague");
+
+        GuardianDataAccessObject guardianDataAccessObject = new GuardianDataAccessObject("PUT API KEY HERE");
+
+        // creates test users and adds them to in memory dao
+        UpdateRankingsUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+        CommonUserFactory userFactory = new CommonUserFactory();
+        User more = userFactory.create("more_points", "p");
+        User less = userFactory.create("less_points", "p");
+        userRepository.save(more);
+        userRepository.save(less);
+
+        // creates test league
         UpdateRankingsLeagueDataAccessInterface leagueRepository = new InMemoryUserDataAccessObject();
-        LeagueFactory factory = new LeagueFactory();
-        League league = factory.create("rankingsTestLeague", new ArrayList<>());
-        league.setWords("more_points", new String[]{"The", "Trump"});
+        LeagueFactory leagueFactory = new LeagueFactory();
+        League league = leagueFactory.create("rankingsTestLeague", new ArrayList<>());
+        league.setWords("more_points", new String[]{"the", "he", "she", "they", "no", "100"});
+        league.setWords("less_points", new String[]{"", "", "", "", "", "-1"});
         leagueRepository.save(league);
 
         UpdateRankingsOutputBoundary successPresenter = new UpdateRankingsOutputBoundary() {
             @Override
-            public void execute(UpdateRankingsOutputData UpdateRankingsOutputData) {
-                //assertEquals();
+            public void execute(UpdateRankingsOutputData updateRankingsOutputData) {
+                ArrayList<User> expectedRanking = new ArrayList<>();
+                expectedRanking.add(more);
+                expectedRanking.add(less);
+                assertEquals(expectedRanking, updateRankingsOutputData.getLiveRankings());
+                assertEquals(expectedRanking, updateRankingsOutputData.getHistoricalRankings());
             }
         };
 
-        //UpdateRankingsInputBoundary interactor = new UpdateRankingsInteractor();
-        //interactor.execute(inputData);
+        UpdateRankingsInputBoundary interactor = new UpdateRankingsInteractor(guardianDataAccessObject,
+                successPresenter, leagueRepository, userRepository);
+        interactor.execute(inputData);
 
     }
 }
