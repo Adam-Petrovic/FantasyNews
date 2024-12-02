2bb7824685949f359afd726ce4636560d60aa024
Evelyn Hughes <evelyn.hughes@mail.utoronto.ca>
2024-12-01 20:45:49 -0500

    created to_league_actions test

--- src/main/java/usecase/to_league_actions/ToLeagueActionsUserDataAccessInterface.java
+++ src/main/java/usecase/to_league_actions/ToLeagueActionsUserDataAccessInterface.java
@@ -2,7 +2,10 @@ package usecase.to_league_actions;
 
 import entity.User;
 
+import java.util.ArrayList;
+
 public interface ToLeagueActionsUserDataAccessInterface {
     public boolean userInLeague(String username, String leagueID);
     public void save(User user);
+    public ArrayList<String> addLeague(String username, String leagueID);
 }
