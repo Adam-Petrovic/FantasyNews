3fee7ab88fa19ac7c5fa3344d0e8c8cc40ad8e86
Adam-Petrovic <adam.petrovic2005@gmail.com>
2024-12-02 02:08:49 -0500

    I UNBREAK

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -121,19 +121,7 @@ import view.leagueviews.DraftView;
 import view.leagueviews.LeagueActionsView;
 import view.leagueviews.LeagueView;
 
-
-/**
- * The AppBuilder class is responsible for putting together the pieces of
- * our CA architecture; piece by piece.
- * <p/>
- * This is done by adding each View and then adding related Use Cases.
- */
-// Checkstyle note: you can ignore the "Class Data Abstraction Coupling"
-//                  and the "Class Fan-Out Complexity" issues for this lab; we encourage
-//                  your team to think about ways to refactor the code to resolve these
-//                  if your team decides to work with this as your starter code
-//                  for your final project this term.
-
+@SuppressWarnings({"checkstyle:ClassDataAbstractionCoupling", "checkstyle:SuppressWarnings", "checkstyle:ClassFanOutComplexity", "checkstyle:LineLength"})
 public class AppBuilder {
     private final JPanel cardPanel = new JPanel();
     private final CardLayout cardLayout = new CardLayout();
