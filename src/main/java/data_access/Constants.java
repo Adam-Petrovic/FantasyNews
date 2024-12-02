2c6a2e7d98a58216e0210af388807eb0cf397244
pratsosa <alexandrospratsos@mail.utoronto.ca>
2024-12-01 23:18:56 -0500

    more checkstyle issues

--- src/main/java/data_access/Constants.java
+++ src/main/java/data_access/Constants.java
@@ -14,4 +14,5 @@ public class Constants {
     public static final String[] DEFAULT_WORDS = {"Default 1", "Default 2", "Default 3", "Default 4", "Default 5", "0"};
     public static final String LEAGUE_ACTIONS_VIEW_NAME = "league actions";
     public static final Integer TEXT_FIELD_LENGTH = 10;
+    public static final Integer TIME = 3;
 }
